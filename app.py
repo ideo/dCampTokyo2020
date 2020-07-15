62820fe6504f323e9e26b4e02d7b679a6bfc3c2e
daidaidais <daisuke4yukita@gmail.com>
2020-07-15 13:12:39 +0900

    Socket IO test

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, make_response, redirect
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@socketio.on("message")
+def handleMessage(data):
+    emit("new_message",data,broadcast=True)
+
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5004)
+    socketio.run(app, debug=True, port=5004)
